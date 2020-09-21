a465f964abff57f9ef19f6e018416a60770ef763
BookYao <workfile_BookYao@163.com>
2020-09-22 00:50:32 +0800

    [update] 删除排序数组中的重复项

--- algorithm/removeDuplicates.c
+++ algorithm/removeDuplicates.c
@@ -24,13 +24,32 @@ int removeDuplicates(int *num, int len)
 }
 
 
+int removeDuplicates1(int* nums, int numsSize){
+    if (!nums || numsSize <= 0) {
+            return 0;
+    }
+
+    int i = 1, res = 1;
+    for (; i < numsSize; i++) {
+        if (nums[i - 1] == nums[i]) {
+            continue;
+        } else {
+            nums[res] = nums[i];
+            res++;
+        }
+    }
+    return res;
+}
+
+
 int main()
 {
     //int num[] = {};
-    //int num[] = {1, 2, 2, 3, 4, 4, 5, 5, 6, 10, 10, 11, 12, 14, 14};
-    int num[] = {0,0,1,1,1,2,2,3,3,4};
+    int num[] = {1, 2, 2, 3, 4, 4, 5, 5, 6, 10, 10, 11, 12, 14, 14};
+    //int num[] = {0,0,1,1,1,2,2,3,3,4};
     int ret = 0;
-    ret = removeDuplicates(num, sizeof(num)/sizeof(int));
+    //ret = removeDuplicates(num, sizeof(num)/sizeof(int));
+    ret = removeDuplicates1(num, sizeof(num)/sizeof(int));
     printf("ret:%d\n", ret);
     int i;
     for (i = 0; i < ret; i++) {
