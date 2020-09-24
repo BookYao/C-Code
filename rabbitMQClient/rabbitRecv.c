363ce19eb4e7d0a0c7665f32cf09aa1b926d129e
BookYao <workfile_BookYao@163.com>
2020-09-24 23:47:51 +0800

    [add] rabbitmq recv msg demo

---
+++ rabbitMQClient/rabbitRecv.c
@@ -0,0 +1,54 @@
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "rabbitmq.h"
+
+
+int main(int argc, char const *const *argv) {
+    if (argc < 4) {
+        fprintf(stderr, "Usage: amqp_listen host port Queue\n");
+        return 1;
+    }
+
+    T_MQ_Conf_Info confInfo;
+    T_MQ_Conn_Info *connInfo;
+    memset(&confInfo, 0, sizeof(T_MQ_Conf_Info));
+
+
+    strncpy(confInfo.host, argv[1], sizeof(confInfo.host) - 1);
+    confInfo.port = atoi(argv[2]);
+    strncpy(confInfo.rmquser, "baseuser", sizeof(confInfo.rmquser) - 1);
+    strncpy(confInfo.rmqpasswd, "basepasswd", sizeof(confInfo.rmqpasswd) - 1);
+
+    strncpy(confInfo.vhost, "vhostBasemq", sizeof(confInfo.exchangeType) - 1);
+    confInfo.channelid = 1;
+
+    connInfo = initMQConn(&confInfo);
+    
+    setExchange(connInfo, "amq.fanout", "fanout");
+    setRouteKey(connInfo, "baseKey");   
+    declareQueue(connInfo, argv[3]);
+    declareQueueAndBindQueue(connInfo, argv[3]);
+
+    int msgLen = 0;
+    char *msg = NULL;
+
+#if 1
+    for (;;) {
+        msgLen = recvMsg(connInfo, &msg);
+        printf("msg:%s-len:%d\n", msg, msgLen);
+        free(msg);
+    }
+#endif
+
+    destoryMQConn(connInfo);
+
+    return 0;
+}
+
+
+
